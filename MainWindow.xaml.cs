ed11a67505b84c1b63743b3fd25c8d1cee5556b5
Andrej Karavida <andrejkaravida@gmail.com>
2020-03-23 00:33:49 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafika
+{
+
+    public partial class MainWindow : Window
+    {
+        public List<UIElement> executed = new List<UIElement>();
+        public List<UIElement> canceled = new List<UIElement>();
+        public List<Point> polygon;
+        public string ObjectSelected { get; set; } = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            polygon = new List<Point>();
+        }
+
+        private void elipse_Click(object sender, RoutedEventArgs e)
+        {
+            ObjectSelected = "Ellipse";
+            elipsebtn.Background = Brushes.LightBlue;
+            rectanglebtn.Background = Brushes.LightGray;
+            polygonbtn.Background = Brushes.LightGray;
+            imagebtn.Background = Brushes.LightGray;
+
+            elipsebtn.BorderThickness = new Thickness(2);
+            imagebtn.BorderThickness = new Thickness(1);
+            rectanglebtn.BorderThickness = new Thickness(1);
+            polygonbtn.BorderThickness = new Thickness(1);
+
+            ClearDots();
+        }
+        private void rectangle_Click(object sender, RoutedEventArgs e)
+        {
+            ObjectSelected = "Rectangle";
+            rectanglebtn.Background = Brushes.LightBlue;
+            elipsebtn.Background = Brushes.LightGray;
+            polygonbtn.Background = Brushes.LightGray;
+            imagebtn.Background = Brushes.LightGray;
+
+            imagebtn.BorderThickness = new Thickness(1);
+            elipsebtn.BorderThickness = new Thickness(1);
+            rectanglebtn.BorderThickness = new Thickness(2);
+            polygonbtn.BorderThickness = new Thickness(1);
+
+            ClearDots();
+        }
+        private void polygon_Click(object sender, RoutedEventArgs e)
+        {
+            ObjectSelected = "Polygon";
+            polygonbtn.Background = Brushes.LightBlue;
+            elipsebtn.Background = Brushes.LightGray;
+            rectanglebtn.Background = Brushes.LightGray;
+            imagebtn.Background = Brushes.LightGray;
+
+            imagebtn.BorderThickness = new Thickness(1);
+            elipsebtn.BorderThickness = new Thickness(1);
+            rectanglebtn.BorderThickness = new Thickness(1);
+            polygonbtn.BorderThickness = new Thickness(2);
+        }
+        private void image_Click(object sender, RoutedEventArgs e)
+        {
+            ObjectSelected = "Image";
+            polygonbtn.Background = Brushes.LightGray;
+            elipsebtn.Background = Brushes.LightGray;
+            rectanglebtn.Background = Brushes.LightGray;
+            imagebtn.Background = Brushes.LightBlue;
+
+            imagebtn.BorderThickness = new Thickness(2);
+            elipsebtn.BorderThickness = new Thickness(1);
+            rectanglebtn.BorderThickness = new Thickness(1);
+            polygonbtn.BorderThickness = new Thickness(1);
+
+            ClearDots();
+        }
+        private void undo_Click(object sender, RoutedEventArgs e)
+        {
+            if (executed.Count != 0)
+            {
+                if (myCanvas.Children.Contains(executed[executed.Count - 1]))
+                {
+                    myCanvas.Children.Remove(executed[executed.Count - 1]);
+                    canceled.Add(executed[executed.Count - 1]);
+                    executed.RemoveAt(executed.Count - 1);
+                }
+                else
+                {
+                    while (!myCanvas.Children.Contains(executed[executed.Count - 1]))
+                    {
+                        myCanvas.Children.Add(executed[executed.Count - 1]);
+                        canceled.Add(executed[executed.Count - 1]);
+                        executed.RemoveAt(executed.Count - 1);
+                        if (executed.Count == 0)
+                            break;
+                    }
+                }
+            }
+            else if(polygon.Count > 0)
+                ClearDots();
+            else
+            {
+                MessageBox.Show("No objects in queue for undo!");
+            }
+
+        }
+        private void redo_Click(object sender, RoutedEventArgs e)
+        {
+            if (canceled.Count != 0)
+            {
+                if (!myCanvas.Children.Contains(canceled[canceled.Count - 1]))
+                {
+                    myCanvas.Children.Add(canceled[canceled.Count - 1]);
+                    executed.Add(canceled[canceled.Count - 1]);
+                    canceled.RemoveAt(canceled.Count - 1);
+                }
+                else
+                {
+                    while (myCanvas.Children.Contains(canceled[canceled.Count - 1]))
+                    {
+                        myCanvas.Children.Remove(canceled[canceled.Count - 1]);
+                        executed.Add(canceled[canceled.Count - 1]);
+                        canceled.RemoveAt(canceled.Count - 1);
+                        if (canceled.Count == 0)
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No objects in queue for redo!");
+            }
+        }
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            ObjectSelected = "";
+            int numOfChildren = myCanvas.Children.Count;
+
+            for (int i = 0; i < numOfChildren; i++)
+            {
+                executed.Add(myCanvas.Children[i]);
+            }
+            ClearDots();
+            myCanvas.Children.Clear();
+
+            elipsebtn.Background = Brushes.LightGray;
+            rectanglebtn.Background = Brushes.LightGray;
+            polygonbtn.Background = Brushes.LightGray;
+            imagebtn.Background = Brushes.LightGray;
+
+            elipsebtn.BorderThickness = new Thickness(1);
+            imagebtn.BorderThickness = new Thickness(1);
+            rectanglebtn.BorderThickness = new Thickness(1);
+            polygonbtn.BorderThickness = new Thickness(1);
+
+        }
+        private void myCanvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var position = Mouse.GetPosition(myCanvas);
+            Point startingPoint = new Point(position.X, position.Y);
+            Point pos = e.GetPosition((Canvas)sender);
+
+            if (ObjectSelected.Length == 0)
+            {
+                MessageBox.Show("Please choose the object first!");
+            }
+            else if(ObjectSelected == "Polygon")
+            {
+
+                Rectangle dot = new Rectangle() { Width = 4, Height = 4, Fill = Brushes.Black };
+
+                if (polygon.Count > 0)
+                {
+                    dot.Fill = Brushes.Black;
+                    dot.Width -= 1;
+                    dot.Height -= 1;
+
+                    Line l = new Line() { X1 = polygon[polygon.Count - 1].X, Y1 = polygon[polygon.Count - 1].Y, X2 = pos.X, Y2 = pos.Y, Stroke = Brushes.Black };
+
+                    myCanvas.Children.Add(l);
+                }
+
+                myCanvas.Children.Add(dot);
+                Canvas.SetLeft(dot, pos.X - dot.Width / 2);
+                Canvas.SetTop(dot, pos.Y - dot.Height / 2);
+
+                polygon.Add(pos);
+            }
+            else
+            {
+                SelectAtributesWindow window = new SelectAtributesWindow(ObjectSelected, startingPoint);
+                window.ShowDialog();
+            }
+        }
+        private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var position = Mouse.GetPosition(myCanvas);
+            Point startingPoint = new Point(position.X, position.Y);
+
+            if (polygon.Count >= 3 && ObjectSelected == "Polygon")
+            {
+                SelectAtributesWindow window = new SelectAtributesWindow("Polygon", startingPoint, null, false, null, polygon);
+                window.ShowDialog();
+            }
+        }
+        public void object_clicked(object sender, EventArgs e)
+        {
+            string objectCalled = sender.ToString().Split('.')[3];
+
+            if(objectCalled.Trim().ToLower() == "ellipse")
+            {
+                Ellipse ellipse = sender as Ellipse;
+                SelectAtributesWindow window = new SelectAtributesWindow(objectCalled.ToLower(), new Point(), ellipse, true);
+                window.ShowDialog();
+
+            }
+            else if(objectCalled.Trim().ToLower() == "rectangle")
+            {
+                Rectangle rectangle = sender as Rectangle;    
+                SelectAtributesWindow window = new SelectAtributesWindow(objectCalled.ToLower(), new Point(), rectangle, true);
+                window.ShowDialog();
+
+            }
+            else if (objectCalled.Trim().ToLower() == "polygon")
+            {
+                Polygon polygon = sender as Polygon;
+                SelectAtributesWindow window = new SelectAtributesWindow(objectCalled.ToLower(), new Point(), polygon, true);
+                window.ShowDialog();
+            }
+            else
+            {
+                Image image = sender as Image;
+                SelectAtributesWindow window = new SelectAtributesWindow(objectCalled.ToLower(), new Point(), null, true, image);
+                window.ShowDialog();
+            }
+
+     
+        }
+        private void ClearDots()
+        {
+            if(polygon.Count > 0)
+            {
+                 myCanvas.Children.RemoveRange(myCanvas.Children.Count - (2 * polygon.Count - 1), 2 * polygon.Count - 1);
+                 polygon.Clear();
+            }
+        }
+    }
+}
---
+++ SelectAtributesWindow.xaml.cs
@@ -0,0 +1,435 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Grafika
+{
+    public partial class SelectAtributesWindow : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string componentType = "";
+        private string headline;
+        private string objwidth;
+        private string objheight;
+        private Color fillColor;
+        private Color borderColor;
+        private string objBorderThickness;
+        private string imagePath;
+
+        public void OnPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string Headline
+        {
+            get
+            {
+                return headline;
+            }
+            set
+            {
+                if (value != headline)
+                {
+                    headline = value;
+                    OnPropertyChanged("Headline");
+                }
+            }
+        }
+        public string ImagePath
+        {
+            get
+            {
+                return imagePath;
+            }
+            set
+            {
+                if (value != imagePath)
+                {
+                    imagePath = value;
+                    OnPropertyChanged("ImagePath");
+                }
+            }
+        }
+        public string ObjWidth
+        {
+            get
+            {
+                return objwidth;
+            }
+            set
+            {
+                if (value != objwidth)
+                {
+                    objwidth = value;
+                    OnPropertyChanged("ObjWidth");
+                }
+            }
+        }
+        public string ObjHeight
+        {
+            get
+            {
+                return objheight;
+            }
+            set
+            {
+                if (value != objheight)
+                {
+                    objheight = value;
+                    OnPropertyChanged("ObjHeight");
+                }
+            }
+        }
+        public Color FillColor
+        {
+            get
+            {
+                return fillColor;
+            }
+            set
+            {
+                if (value != fillColor)
+                {
+                    fillColor = value;
+                    OnPropertyChanged("FillColor");
+                }
+            }
+        }
+        public Color BorderColor { get => borderColor; set => borderColor = value; }
+        public string ObjBorderThickness
+        {
+            get
+            {
+                return objBorderThickness;
+            }
+            set
+            {
+                if (value != objBorderThickness)
+                {
+                    objBorderThickness = value;
+                    OnPropertyChanged("ObjBorderThickness");
+                }
+            }
+        }
+        public double StartingX { get; set; }
+        public double StartingY { get; set; }
+        public Image CanvasImage { get; set; }
+        public Shape Shape { get; set; }
+        public Image ImageSelected { get; set; }
+        public List<Point> PolygonPoints { get; set; }
+
+        public SelectAtributesWindow(string cmpType, Point startingCoordinates, Shape shape = null, bool isChange = false, Image image = null, List<Point> points = null)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            componentType = cmpType;
+            cmbBox1.ItemsSource = typeof(Colors).GetProperties();
+            cmbBox2.ItemsSource = typeof(Colors).GetProperties();
+            cmbBox1.SelectedIndex = 3;
+            cmbBox2.SelectedIndex = 4;
+            StartingX = startingCoordinates.X;
+            StartingY = startingCoordinates.Y;
+          
+            if (points != null)
+                PolygonPoints = points;
+
+            if (cmpType.ToLower() == "polygon")
+            {
+                objWidth.Visibility = Visibility.Hidden;
+                objHeight.Visibility = Visibility.Hidden;
+                widthtextblock.Visibility = Visibility.Hidden;
+                heighttextblock.Visibility = Visibility.Hidden;
+            }
+
+            if (cmpType.ToLower() == "ellipse")
+            {
+                widthtextblock.Text = "RadiusX:";
+                heighttextblock.Text = "RadiusY:";
+            }
+
+            if (cmpType.ToLower() == "image")
+            {
+                cmbBox1.Visibility = Visibility.Hidden;
+                cmbBox2.Visibility = Visibility.Hidden;
+                objborderThickness.Visibility = Visibility.Hidden;
+                borderthicknesstextblock.Visibility = Visibility.Hidden;
+                bordercolortextblock.Visibility = Visibility.Hidden;
+                fillcolortextblock.Visibility = Visibility.Hidden;
+                objborderThickness.Visibility = Visibility.Hidden;
+                borderthicknesstextblock.Visibility = Visibility.Hidden;
+            }
+
+            if (isChange)
+            {
+                Headline = "Change a " + cmpType;
+                changebtn.Visibility = Visibility.Visible;
+                drawbtn.Visibility = Visibility.Hidden;
+                objWidth.IsReadOnly = true;
+                objHeight.IsReadOnly = true;
+
+                if (shape != null)
+                {
+                    Shape = shape;
+                    loadimagebutton.Visibility = Visibility.Hidden;
+                    ObjBorderThickness = shape.StrokeThickness.ToString();
+                    ObjWidth = shape.Width.ToString();
+                    ObjHeight = shape.Height.ToString();
+                    cmbBox1.SelectedIndex = Int32.Parse(shape.Name.Split('_')[1]);
+                    cmbBox2.SelectedIndex = Int32.Parse(shape.Name.Split('_')[2]);
+
+                }
+                else
+                {
+                    ImageSelected = image;
+                    ObjWidth = image.Width.ToString();
+                    ObjHeight = image.Height.ToString();              
+                }
+            }
+            else
+            {
+                Headline = "Draw a " + cmpType;
+                changebtn.Visibility = Visibility.Hidden;
+                drawbtn.Visibility = Visibility.Visible;
+
+                if(cmpType.ToLower() != "image")
+                    loadimagebutton.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void draw_Click(object sender, RoutedEventArgs e)
+        {
+            bool validationError = false;
+
+            if(objWidth.Text == "" && objWidth.Visibility == Visibility.Visible)
+            {
+                objWidth.BorderBrush = Brushes.Red;
+                validationError = true;
+            }
+            else
+                objWidth.BorderBrush = Brushes.Black;
+
+            if (objHeight.Text == "" && objHeight.Visibility == Visibility.Visible)
+            {
+                objHeight.BorderBrush = Brushes.Red;
+                validationError = true;
+            }
+            else
+                objHeight.BorderBrush = Brushes.Black;
+
+            if (objborderThickness.Visibility == Visibility.Visible && objborderThickness.Text == "")
+            {
+                objborderThickness.BorderBrush = Brushes.Red;
+                validationError = true;
+            }
+            else
+                objborderThickness.BorderBrush = Brushes.Black;
+
+            double result1;
+            double result2;
+            double result3;
+
+            if ((!Double.TryParse(objWidth.Text, out result1) || result1 <= 0) && objWidth.Visibility == Visibility.Visible)
+            {
+                validationError = true;
+                objWidth.BorderBrush = Brushes.Red;
+            }
+            else
+                objWidth.BorderBrush = Brushes.Black;
+
+            if ((!Double.TryParse(objHeight.Text, out result2) || result2 <= 0) && objHeight.Visibility == Visibility.Visible)
+            {
+                validationError = true;
+                objHeight.BorderBrush = Brushes.Red;
+            }
+            else
+                objHeight.BorderBrush = Brushes.Black;
+
+            if ((!Double.TryParse(objborderThickness.Text, out result3) || result3 <= 0) && objborderThickness.Visibility == Visibility.Visible)
+            {
+                validationError = true;
+                objborderThickness.BorderBrush = Brushes.Red;
+            }
+            else
+                objborderThickness.BorderBrush = Brushes.Black;
+
+            if (validationError)
+                MessageBox.Show("You need to enter valid data !");
+            else
+            {
+                MainWindow main = ((MainWindow)Application.Current.MainWindow);
+                SolidColorBrush fillColor = new SolidColorBrush(FillColor);
+                SolidColorBrush borderColor = new SolidColorBrush(BorderColor);
+
+                if(componentType == "Rectangle")
+                {
+                    Rectangle rectangle = new Rectangle();
+
+                    rectangle.Name = "rectangle_" + cmbBox1.SelectedIndex.ToString() + "_" + cmbBox2.SelectedIndex.ToString();
+                    rectangle.Fill = fillColor;
+                    rectangle.Stroke = borderColor;
+                    rectangle.Width = Double.Parse(objWidth.Text);
+                    rectangle.Height = Double.Parse(objHeight.Text);
+                    rectangle.StrokeThickness = Double.Parse(objborderThickness.Text);
+                    main.myCanvas.Children.Add(rectangle);
+                    main.executed.Add(rectangle);
+                    Canvas.SetLeft(rectangle, StartingX);
+                    Canvas.SetTop(rectangle, StartingY);                             
+                    rectangle.MouseLeftButtonUp += main.object_clicked;
+
+                    this.Close();
+                }
+
+                if(componentType == "Ellipse")
+                {
+                    Ellipse ellipse = new Ellipse();
+
+                    ellipse.Name = "ellipse_" + cmbBox1.SelectedIndex.ToString() + "_" + cmbBox2.SelectedIndex.ToString();
+                    ellipse.Fill = fillColor;
+                    ellipse.Stroke = borderColor;
+                    ellipse.Width = Double.Parse(objWidth.Text);
+                    ellipse.Height = Double.Parse(objHeight.Text);
+                    ellipse.StrokeThickness = Double.Parse(objborderThickness.Text);
+                    main.myCanvas.Children.Add(ellipse);
+                    main.executed.Add(ellipse);
+                    Canvas.SetLeft(ellipse, StartingX);
+                    Canvas.SetTop(ellipse, StartingY);
+                    ellipse.MouseLeftButtonUp += main.object_clicked;
+                   
+                    this.Close();
+                }
+
+                if(componentType == "Polygon")
+                {
+
+                    Polygon polygon = new Polygon();
+
+                    foreach (Point point in PolygonPoints)
+                    {
+                        polygon.Points.Add(point);
+                    }
+
+                    polygon.Name = "polygon_" + cmbBox1.SelectedIndex.ToString() + "_" + cmbBox2.SelectedIndex.ToString();
+                    polygon.Fill = fillColor;
+                    polygon.Stroke = borderColor;
+                    polygon.StrokeThickness = Double.Parse(objborderThickness.Text);
+                            
+                    main.myCanvas.Children.Add(polygon);
+                    main.executed.Add(polygon);
+                    polygon.MouseLeftButtonUp += main.object_clicked;
+
+                    main.myCanvas.Children.RemoveRange(main.myCanvas.Children.Count - 2 * main.polygon.Count, 2 * main.polygon.Count - 1);
+                    main.polygon.Clear();
+
+                    this.Close();
+                }
+
+                if(componentType == "Image")
+                {
+                    if(CanvasImage != null)
+                    {
+                        CanvasImage.Width = Double.Parse(objWidth.Text);
+                        CanvasImage.Height = Double.Parse(objHeight.Text);
+                        CanvasImage.Stretch = Stretch.Fill;
+                        main.myCanvas.Children.Add(CanvasImage);
+                        main.executed.Add(CanvasImage);
+                        Canvas.SetLeft(CanvasImage, StartingX);
+                        Canvas.SetTop(CanvasImage, StartingY);
+                        CanvasImage.MouseLeftButtonUp += main.object_clicked;
+                        CanvasImage.Name = "image_" + StartingX + "_" + StartingY;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select a valid image!");
+                    }
+
+                    this.Close();
+                }
+            }
+        }
+
+        private void loadimagebutton_Click(object sender, RoutedEventArgs e)
+        {
+            CanvasImage = new Image();
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (.jpg;.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (.png)|.png";
+            if (op.ShowDialog() == true)
+            {
+                CanvasImage.Source = new BitmapImage(new Uri(op.FileName));
+                string[] words = CanvasImage.Source.ToString().Split('/');
+                int length = words.Length;
+                ImagePath = words[length-1];
+            }
+        }
+
+        private void cmbBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FillColor = (Color)(cmbBox1.SelectedItem as PropertyInfo).GetValue(null, null);
+        }
+
+        private void cmbBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BorderColor = (Color)(cmbBox2.SelectedItem as PropertyInfo).GetValue(null, null);
+        }
+
+        private void change_Click(object sender, RoutedEventArgs e)
+        {
+            bool validationError = false;
+
+            if (objborderThickness.Visibility == Visibility.Visible && objborderThickness.Text == "")
+            {
+                objborderThickness.BorderBrush = Brushes.Red;
+                validationError = true;
+            }
+            else
+                objborderThickness.BorderBrush = Brushes.Black;
+
+            double result;
+            
+            if ((!Double.TryParse(objborderThickness.Text, out result) || result <= 0) && objborderThickness.Visibility == Visibility.Visible)
+            {
+                validationError = true;
+                objborderThickness.BorderBrush = Brushes.Red;
+            }
+            else
+                objborderThickness.BorderBrush = Brushes.Black;
+
+            if(validationError)
+                MessageBox.Show("You need to enter valid data !");
+            else 
+            {
+                SolidColorBrush fillColor = new SolidColorBrush(FillColor);
+                SolidColorBrush borderColor = new SolidColorBrush(BorderColor);
+
+                if (componentType != "image")
+                {                
+                    Shape.Fill = fillColor;
+                    Shape.Stroke = borderColor;
+                    Shape.StrokeThickness = Double.Parse(objborderThickness.Text);
+                    Shape.Name = "shape_" + cmbBox1.SelectedIndex.ToString() + "_" + cmbBox2.SelectedIndex.ToString();
+                }             
+                else
+                {
+                    ImageSelected.Source = CanvasImage.Source;
+                }
+
+                Close();
+            }
+        }
+    }
+}
